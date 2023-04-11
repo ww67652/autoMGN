11e0a2c1235c730be508fbdfbf841fdf918d760b
ww67652 <1749113286@qq.com>
2023-04-11 22:43:31 +0800

    ww

--- autoMGN/train.py
+++ autoMGN/train.py
@@ -11,6 +11,8 @@ from tools.dataset2 import Dataset
 from tools.common import Accumulator
 from model.MGN import MGN
 from config.config import load_train_config
+import matplotlib.pyplot as plt
+
 
 import warnings
 
@@ -37,7 +39,13 @@ def accumulate(model, dataloader, config):
 
 
 def train(model, train_dataloader, valid_dataloader, criterion, optimizer, scheduler, config):
+
+    warnings.filterwarnings("ignore", message="The NumPy module was reloaded")
+    warnings.filterwarnings("ignore", message="Using a target size")
+
     log = open(os.path.join(config['log_root'], 'log.txt'), 'a')
+    train_losses = []
+    train_mses = []
 
     accumulate(model, train_dataloader, config)
     for epoch in range(config['last_epoch'] + 1, config['max_epoch'] + 1):
@@ -62,7 +70,7 @@ def train(model, train_dataloader, valid_dataloader, criterion, optimizer, sched
             optimizer.zero_grad()
 
             prediction = model(senders, receivers, nodes, edges)
-            print(prediction.size())
+            # print(prediction.size())
             loss = criterion(prediction, model.output_normalizer(output))
             loss.backward()
             epoch_loss += loss.item()
@@ -72,7 +80,13 @@ def train(model, train_dataloader, valid_dataloader, criterion, optimizer, sched
             scheduler.step()
 
         end = time.perf_counter()
-        print('Train Loss: %f, MSE: %f' % (epoch_loss / len(train_dataloader), epoch_mse / len(train_dataloader)))
+        train_loss = epoch_loss / len(train_dataloader)
+        train_mse = epoch_mse / len(train_dataloader)
+        train_losses.append(train_loss)
+        train_mses.append(train_mse)
+
+        # Print and write logs
+        print('Train Loss: %f, MSE: %f' % (train_loss, train_mse))
         print('Train Time: %f' % (end - start))
 
         log.write('Train Loss: %f, MSE: %f' % (epoch_loss / len(train_dataloader), epoch_mse / len(train_dataloader)) + '\n')
@@ -113,12 +127,40 @@ def train(model, train_dataloader, valid_dataloader, criterion, optimizer, sched
         if epoch % config['save_steps'] == 0:
             torch.save(copy.deepcopy((model.state_dict())), os.path.join(config['ckpt_root'], '%d.pkl' % epoch))
 
+    # Plot the train loss curve
+    plt.ylim([0, 1])
+    plt.plot(range(0, config['max_epoch'] + 1), train_losses)
+    plt.title('Train Loss Curve')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.savefig(os.path.join(config['log_root'], 'train_loss_curve.png'))
+
+    # 创建画布和两个坐标轴对象
+    # fig, ax1 = plt.subplots()
+    # ax2 = ax1.twinx()
+    #
+    # # 绘制第一条曲线，并设置纵坐标轴范围和颜色
+    # ax1.plot(range(0, config['max_epoch'] + 1), train_losses, color='blue')
+    # ax1.set_ylim([0, 1])
+    # ax1.set_ylabel('lose', color='blue')
+    #
+    # # 绘制第二条曲线，并设置纵坐标轴范围和颜色
+    # ax2.plot(range(0, config['max_epoch'] + 1), train_mses, color='red')
+    # ax2.set_ylim([0, 0.002])
+    # ax2.set_ylabel('mse', color='red')
+    #
+    # # 设置横坐标轴标签和标题
+    # ax1.set_xlabel('Epoch')
+    # plt.title('Loss & MSE for training model')
+    #
+    # # 显示图形
+    # # plt.show()
+    # plt.savefig(os.path.join(config['log_root'], 'Loss&MSE.png'))
+
     return
 
 
 if __name__ == '__main__':
-
-    warnings.filterwarnings("ignore", message="The NumPy module was reloaded")
 
     config = load_train_config()
     random.seed(config['seed'])
@@ -135,8 +177,8 @@ if __name__ == '__main__':
     train_dataset = Dataset(config['dataset'], parts=[0, 1, 2, 3, 4, 5], npart=7, ids=['JOB1'])
     valid_dataset = Dataset(config['dataset'], parts=[6], npart=7, ids=['JOB1'])
 
-    train_dataloader = data.DataLoader(train_dataset, batch_size=config['batch_size'], num_workers=4, pin_memory=True)
-    valid_dataloader = data.DataLoader(valid_dataset, batch_size=config['batch_size'], num_workers=4, pin_memory=True)
+    train_dataloader = data.DataLoader(train_dataset, batch_size=config['batch_size'], num_workers=2, pin_memory=True)
+    valid_dataloader = data.DataLoader(valid_dataset, batch_size=config['batch_size'], num_workers=2, pin_memory=True)
 
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=config['lr'])
